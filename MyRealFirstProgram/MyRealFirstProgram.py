83930ffaf2380d193825d941ff7299099d2e6887
RomanF7 <rferreira@estudiante.ceibal.edu.uy>
2022-06-02 08:02:29 -0300

    A different mode V1.1

--- MyRealFirstProgram/MyRealFirstProgram.py
+++ MyRealFirstProgram/MyRealFirstProgram.py
@@ -3,3 +3,8 @@ number1 = int(input())
 print("Introduce the second number: ")
 number2 = int(input())
 print("The sum between ",number1," + ",number2," = ",(number1+number2))
+
+#!Update. A different mode.
+
+number1 = int(input("Introduce the first number: "))
+number2 = int(input("Introduce the second number: "))
