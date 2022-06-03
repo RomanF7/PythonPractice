740b26cd0e04186abff916edbfd91bc17fbbe3fb
RomanF7 <rferreira@estudiante.ceibal.edu.uy>
2022-06-03 14:41:21 -0300

    Vocabulary and V1.2

---
+++ Calculator/BasicCalculator.py
@@ -0,0 +1,7 @@
+while True:
+    print("1-Sum")
+    print("2-Subtract")
+    print("3-Multiplication")
+    print("4-Division")
+    print("0-Exit")
+    op = int(input("Enter the option: "))
--- MyRealFirstProgram/MyRealFirstProgram.py
+++ MyRealFirstProgram/MyRealFirstProgram.py
@@ -1,10 +1,10 @@
-print("Introduce the first number: ")
+print("Enter the first number: ")
 number1 = int(input())
-print("Introduce the second number: ")
+print("Enter the second number: ")
 number2 = int(input())
 print("The sum between ",number1," + ",number2," = ",(number1+number2))
 
 #!Update. A different mode.
 
-number1 = int(input("Introduce the first number: "))
-number2 = int(input("Introduce the second number: "))
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
