02620268e6a7cc50726b7971aae982a5d2acd030
RomanF7 <rferreira@estudiante.ceibal.edu.uy>
2022-06-03 15:32:02 -0300

    V1.3

--- Calculator/BasicCalculator.py
+++ Calculator/BasicCalculator.py
@@ -5,3 +5,29 @@ while True:
     print("4-Division")
     print("0-Exit")
     op = int(input("Enter the option: "))
+
+    match op:
+        case 1:
+            number1 = int(input("Enter the first number: "))
+            number2 = int(input("Enter the second number: "))
+            print(number1, " + ", number2, " = ", number1 + number2)
+        case 2:
+            number1 = int(input("Enter the first number: "))
+            number2 = int(input("Enter the second number: "))
+            print(number1, " - ", number2, " = ", number1 - number2)
+        case 3:
+            number1 = int(input("Enter the first number: "))
+            number2 = int(input("Enter the second number: "))
+            print(number1, " X ", number2, " = ", number1 * number2)
+        case 4:
+            number1 = int(input("Enter the first number: "))
+            number2 = int(input("Enter the second number: "))
+            if number2 == 0:
+                print("Not include 0 (zero)")
+                number1 = int(input("Enter the first number: "))
+                number2 = int(input("Enter the second number: "))
+                print(number1," / ",number2," = ",number1 / number2)
+            else:
+                print(number1," / ",number2," = ",number1 / number2)
+        case _:
+            print("Option not exist")
